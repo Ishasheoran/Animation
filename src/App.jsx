95a8e09e3e600a3492c205dd2a94d9eb70e6c712
Isha <ishasheoran46@gmail.com>
2025-11-20 23:36:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Carousel3D from "./Carousel3D";
+import {Navbar,Footer} from "./Navbar";
+function App() {
+  return (
+    <>
+    <Navbar />
+           <Carousel3D />
+<Footer />
+      {/* <div style={{ height: "0px" }}></div>  */}
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,23 @@
+export function Navbar(){
+    return(<>
+    <div className="Navbar">
+        <p>ON-SCROLL 3D CAROUSEL</p>
+        <div className="links">
+            <a href="https://tympanus.net/codrops/2025/05/07/on-scroll-3d-carousel/"  target="_blank" >ARTICLE</a>
+            <a href="https://github.com/codrops/3DCarousel/">CODE</a><a href="https://tympanus.net/codrops/hub/">ALL DEMOS</a>
+        </div>
+    </div>
+    </>)
+}
+export function Footer(){
+    return(<>
+    <div className="Footer">
+        <div className="links">
+            <a href="https://tympanus.net/codrops/hub/tag/3d/">#3D</a>
+            <a href="https://tympanus.net/codrops/hub/tag/carousel/">#CAROUSEL</a>
+            <a href="https://tympanus.net/codrops/hub/tag/page-transition/">#PAGE-TRANSITION</a>
+        </div>
+        <p>Focus on your clients, leave hosting config to Cloudways. Get 50% OFF for 3 months + 50 Free Migrations.</p>
+    </div>
+    </>)
+}
